de0854ef821c1c987acf5378faf55620908299f6
Harshithhk <dota2kelkar@gmail.com>
2025-10-04 06:18:36 -0400

    try fix install jenkins

--- scripts/jenkins-jcasc-setup.sh
+++ scripts/jenkins-jcasc-setup.sh
@@ -10,7 +10,7 @@ echo "*                                                                        *
 echo "**************************************************************************"
 
 sudo apt update
-sudo apt install fontconfig openjdk-21-jre
+sudo apt install fontconfig openjdk-21-jre -y
 
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
--- variables.pkr.hcl
+++ variables.pkr.hcl
@@ -10,5 +10,5 @@ variable region {
 
 variable instance_type {
   type    = string
-  default = "t3.micro"
+  default = "t3.small"
 }
